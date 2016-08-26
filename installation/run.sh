4faa37e321dc24e87b71477123ef545498b059d0
gggeek <ggiunta@kaliop.com>
2016-08-26 01:19:30 +0100

    Add ezpublish install (WIP)

--- installation/run.sh
+++ installation/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # This script is supposed to set up the whole installation:
+
 # 1. build docker images
 # 2. install ezpublish
 
@@ -15,4 +16,17 @@ php stack.php build
 
 php stack.php run
 
-docker exec ezdeploly_cli composer install --no-scripts
+docker exec ezdeploy_cli ./bin/getcomposer.sh
+docker exec ezdeploy_cli composer install --no-scripts
+
+# fix character set of default db created by the standard docker container
+mysql -hmysql -uuser_ezdeploy -pNotSoSecret -e 'drop database ezdeploy'
+mysql -hmysql -uuser_ezdeploy -pNotSoSecret -e 'create database ezdeploy character set utf8'
+
+# import database
+
+# TODO
+
+# symlink legacy settings
+
+# TODO
---
+++ site/web/index.php
@@ -0,0 +1,80 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\Debug\Debug;
+
+// Environment is taken from "ENVIRONMENT" variable, if not set, defaults to "prod"
+$environment = getenv( "ENVIRONMENT" );
+if ( $environment === false )
+{
+    $environment = "prod";
+}
+
+// Depending on the USE_DEBUGGING environment variable, tells whether Symfony should be loaded with debugging.
+// If not set it is activated if in "dev" environment.
+if ( ( $useDebugging = getenv( "USE_DEBUGGING" ) ) === false )
+{
+    $useDebugging = $environment === "dev";
+}
+
+if ( $useDebugging )
+{
+    $loader = require_once __DIR__ . '/../ezpublish/autoload.php';
+}
+else
+{
+    $loader = require_once __DIR__ . '/../ezpublish/bootstrap.php.cache';
+}
+
+// Depending on the USE_APC_CLASSLOADER environment variable, use APC for autoloading to improve performance.
+// If not set it is not used.
+if ( getenv( "USE_APC_CLASSLOADER" ) )
+{
+    $prefix = getenv( "APC_CLASSLOADER_PREFIX" );
+
+    $loader = new ApcClassLoader( $prefix ?: "ezpublish", $loader );
+    $loader->register( true );
+}
+
+require_once __DIR__ . '/../ezpublish/EzPublishKernel.php';
+
+if ( $useDebugging )
+{
+    // NB: reduced the error level when in debug mode because 2014.11 is not perfectly compatible with sf 2.8...
+    Debug::enable(E_ALL ^ E_STRICT);
+}
+
+$kernel = new EzPublishKernel( $environment, $useDebugging );
+
+// we don't want to use the classes cache if we are in a debug session
+if ( !$useDebugging )
+{
+    $kernel->loadClassCache();
+}
+
+// Depending on the USE_HTTP_CACHE environment variable, tells whether the internal HTTP Cache mechanism is to be used.
+// If not set it is activated if not in "dev" environment.
+if ( ( $useHttpCache = getenv( "USE_HTTP_CACHE" ) ) === false )
+{
+    $useHttpCache = $environment !== "dev";
+}
+// Load HTTP Cache ...
+if ( $useHttpCache )
+{
+    require_once __DIR__ . '/../ezpublish/EzPublishCache.php';
+    $kernel = new EzPublishCache( $kernel );
+}
+
+$request = Request::createFromGlobals();
+
+// If you are behind one or more trusted reverse proxies, you might want to set them in TRUSTED_PROXIES environment
+// variable in order to get correct client IP
+if ( ( $trustedProxies = getenv( "TRUSTED_PROXIES" ) ) !== false )
+{
+    Request::setTrustedProxies( explode( ",", $trustedProxies ) );
+}
+
+$response = $kernel->handle( $request );
+$response->send();
+$kernel->terminate( $request, $response );
